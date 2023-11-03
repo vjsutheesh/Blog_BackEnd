160ccb0bc3bcc0e82b5523875f7f8deb7dd038f4
vjsutheesh <vjsutheesh@gmail.com>
2023-11-04 00:00:08 +0530

    updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((error,req,res,next)=>{
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("DATABASE CONNECTED")
     app.listen(process.env.PORT || 5000, () => {
-        console.log(`Server is running at https://vj-blog.onrender.com`);
+        console.log(`Server is running `);
     });
 }).catch(err=>{
      console.log(err)
