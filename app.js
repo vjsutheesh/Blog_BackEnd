8f18aa572450ddfbb667858f0267172345d31924
vjsutheesh <vjsutheesh@gmail.com>
2023-11-04 17:36:44 +0530

    origin updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express()
 app.use(bodyParser.json())
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Origin','https://vj-blog.netlify.app')
     res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization')
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE')
     next();
